a762899b3f5a4ba28ac2eb2ad2c4f1704cffaad5
SerhiyZheliznjak <zheliznjak@gmail.com>
2018-03-01 20:58:32 +0200

    2

--- src/services/NominationService.js
+++ src/services/NominationService.js
@@ -70,7 +70,7 @@ class NominationService {
         return nowInSeconds - recentMatch.start_time < Constants_1.default.MATCH_DUE_TIME_SEC;
     }
     hasNewMatches(playerRecentMatches, recentMatchesCache) {
-        return recentMatchesCache.get(playerRecentMatches.account_id)
+        return recentMatchesCache && recentMatchesCache.get(playerRecentMatches.account_id)
             .reduce((exist, match_id) => exist || playerRecentMatches.recentMatchesIds.indexOf(match_id) < 0, false);
     }
     awardWinners(scoreBoard) {
