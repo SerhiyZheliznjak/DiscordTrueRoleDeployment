9fd71cfd8bd0d97fff4a4f9d32bac94b33855089
Tee Hee <zheliznjak@gmail.com>
2018-03-15 19:17:00 +0200

    converted key to number

--- src/model/json/NominationResultJson.js
+++ src/model/json/NominationResultJson.js
@@ -6,7 +6,10 @@ class NominationResultJson {
         this.owner_account_id = owner_account_id;
         this.score = score;
         this.timeClaimed = timeClaimed;
-        this.key = nominationName;
+        this.key = nominationName.split("").reduce((a, b) => {
+            a = ((a << 5) - a) + b.charCodeAt(0);
+            return a & a;
+        }, 0);
     }
 }
 exports.default = NominationResultJson;
