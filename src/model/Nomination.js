b45c7552f9edce27ab8b1615bbd7d6cf666d0d40
Tee Hee <zheliznjak@gmail.com>
2018-03-24 22:27:38 +0200

    dgd

--- src/model/Nomination.js
+++ src/model/Nomination.js
@@ -39,7 +39,7 @@ class Nomination {
         return this.msg;
     }
     hasHigherScoreThen(that) {
-        console.log(`new winner score: `, this.getScore(), ' stored winner score: ', that.getScore());
+        console.log(this.name, `: new winner score: `, this.getScore(), ' stored winner score: ', that.getScore());
         return this.getScore() > that.getScore();
     }
     isScored() {
