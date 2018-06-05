73c468672e7a7809a173f2a6bfc56d622b8370da
SerhiyZheliznjak <zheliznjak@gmail.com>
2018-06-05 12:10:52 +0300

    fixed dota parser rare issues

--- src/services/DotaParser.js
+++ src/services/DotaParser.js
@@ -2,13 +2,19 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 class DotaParser {
     static getPlayerInfo(match, player_slot) {
-        return !!match ? match.players.find(player => player.player_slot === player_slot) : undefined;
+        if (match && match.players) {
+            return match.players.find(player => player.player_slot === player_slot);
+        }
+        return undefined;
     }
     static getObjectives(match) {
         return !!match ? match.objectives : undefined;
     }
     static getPlayerSlot(match, account_id) {
-        const player = !!match ? match.players.find(p => p.account_id === account_id) : null;
+        let player;
+        if (match && match.players) {
+            player = match.players.find(p => p.account_id === account_id);
+        }
         if (player) {
             return player.player_slot;
         }
