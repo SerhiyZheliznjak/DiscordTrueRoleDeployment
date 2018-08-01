a63e234adec9941c9b5d73f5185c6e470312a1d7
SerhiyZheliznjak <zheliznjak@gmail.com>
2018-08-01 14:21:59 +0300

    test

--- src/model/commands/WinRateCommand.js
+++ src/model/commands/WinRateCommand.js
@@ -18,8 +18,8 @@ class WinRate extends Command_1.CommandBase {
         if (arr.length === 0) {
             this.alreadyProcessing = true;
             this.dataStore.registeredPlayers.subscribe((registeredPlayers) => {
-                console.log('got registered players', registeredPlayers.size);
                 const profileIds = Object.keys(registeredPlayers).map(stringId => +stringId);
+                console.log('got registered players', profileIds.length);
                 rxjs_1.Observable.forkJoin(profileIds.map(account_id => this.mapAccountIdToWinRate(account_id, this.dataStore.getWinLoss(account_id)))).subscribe(accWinRate => this.sendMessage(msg, accWinRate));
             });
         }
