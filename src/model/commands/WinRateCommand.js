f5c28140ffe83b9c3eaf6e72b931066cd7e31594
Serhiy Zheliznyak <zheliznjak@gmail.com>
2018-08-03 21:16:02 +0300

    s

--- src/model/commands/WinRateCommand.js
+++ src/model/commands/WinRateCommand.js
@@ -93,12 +93,13 @@ class WinRate extends Command_1.CommandBase {
     sendMessage(msg, accWinRates, messageHeader) {
         rxjs_1.Observable.forkJoin(accWinRates.map(awr => this.populateWithName(awr)))
             .subscribe(winrates => {
+            const maxCountLength = String(Math.max(...winrates.map(wr => wr.count))).length;
             const winratesMsg = winrates.filter(wr => !isNaN(wr.winRate)).sort((a, b) => b.winRate - a.winRate)
                 .reduce((message, wr) => {
                 const sign = wr.winRate > 50 ? '+' : '-';
                 const palyerName = accWinRates.length > 1 ? ': ' + wr.name : '';
                 return message + sign + ' ' + DiscordUtils_1.DiscordUtils.getPercentString(wr.winRate) + ' з '
-                    + wr.count + palyerName + '\n';
+                    + DiscordUtils_1.DiscordUtils.fillWithSpaces(String(wr.count), maxCountLength) + palyerName + '\n';
             }, '```diff\n' + messageHeader + '\n');
             msg.channel.send(winratesMsg + '#тайтаке```');
             this.unlock();
