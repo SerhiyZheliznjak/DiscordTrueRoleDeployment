168bc0c2ffbe272b2dd4624558b2ea40cd6666d1
Serhiy Zheliznyak <zheliznjak@gmail.com>
2018-08-03 23:49:21 +0300

    d

--- src/model/commands/TopTeams.js
+++ src/model/commands/TopTeams.js
@@ -6,6 +6,9 @@ class TopTeams extends Command_1.CommandBase {
     constructor() {
         super(...arguments);
         this.defaultN = 5;
+        this.nameText = 'Команда';
+        this.winrateText = 'Вінрейт';
+        this.sumText = 'Матчів';
     }
     process(msg) {
         if (!this.isLocked(msg)) {
@@ -17,23 +20,41 @@ class TopTeams extends Command_1.CommandBase {
                     const placeText = this.getPlaceText(index);
                     return t.name.length + placeText.length;
                 })));
-                let nameText = 'Команда';
-                const winrateText = 'Вінрейт';
-                const sumText = 'Матчів';
-                if (maxNameLength > nameText.length) {
-                    nameText = DiscordUtils_1.DiscordUtils.fillWithSpaces(nameText, maxNameLength);
+                if (maxNameLength > this.nameText.length) {
+                    this.nameText = DiscordUtils_1.DiscordUtils.fillWithSpaces(this.nameText, maxNameLength);
                 }
                 const message = topTeams.reduce((msg, team) => {
                     const winrate = DiscordUtils_1.DiscordUtils.getPercentString(Math.round(team.wins / (team.losses + team.wins) * 10000) / 100);
-                    return msg + DiscordUtils_1.DiscordUtils.fillWithSpaces(this.getPlaceText(topTeams.indexOf(team)) + team.name, nameText.length) + ' | '
-                        + DiscordUtils_1.DiscordUtils.fillWithSpaces(String(winrate), winrateText.length) + ' | ' + team.losses + team.wins + '\n';
-                }, '```' + nameText + ' | ' + winrateText + ' | ' + sumText + '\n') + '```';
-                msg.channel.send(this.trimMessage(message));
+                    return msg + DiscordUtils_1.DiscordUtils.fillWithSpaces(this.getPlaceText(topTeams.indexOf(team)) + team.name, this.nameText.length) + ' | '
+                        + DiscordUtils_1.DiscordUtils.fillWithSpaces(String(winrate), this.winrateText.length) + ' | ' + team.losses + team.wins + '\n';
+                }, '');
+                this.sendMessage(msg, message.split('\n'), 0);
                 if (!this.hasNaVi(topTeams)) {
                     msg.channel.send('```cs\n#НАВІ В КАНАВІ```');
                 }
             });
         }
+    }
+    sendMessage(msg, message, lastIndex) {
+        if (message.join('\n').length > 1994 - this.getTableHead().length) {
+            let part = '';
+            let i = lastIndex;
+            while ((part + message[i]).length < 1994 - this.getTableHead().length) {
+                part += message[i] + '\n';
+                i++;
+            }
+            msg.channel.send(this.wrapMessage(part));
+            this.sendMessage(msg, message.slice(i), i);
+        }
+        else {
+            msg.channel.send(this.wrapMessage(message.join('\n')));
+        }
+    }
+    getTableHead() {
+        return this.nameText + ' | ' + this.winrateText + ' | ' + this.sumText + '\n';
+    }
+    wrapMessage(message) {
+        return '```' + this.getTableHead() + message + '```';
     }
     trimMessage(msg) {
         return msg.length < 2000 ? msg : msg.slice(0, 1990) + '```';
