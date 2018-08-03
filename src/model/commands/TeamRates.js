d9dacdb5eec94eaa2c6ea9baee2beda333c3aa1b
Serhiy Zheliznyak <zheliznjak@gmail.com>
2018-08-03 21:10:25 +0300

    d

--- src/model/commands/TeamRates.js
+++ src/model/commands/TeamRates.js
@@ -9,11 +9,11 @@ class TeamRates extends Command_1.CommandBase {
                 const digits = this.getArgs(msg.content.toLowerCase()).find(arg => /\d+/.test(arg));
                 const n = !digits ? 10 : +digits.match(/\d+/)[0];
                 const topTeams = teams.slice(0, n);
-                const maxNumberLength = Math.max(...(topTeams.map(t => t.wins + t.losses)));
+                const maxNumberLength = String(Math.max(...(topTeams.map(t => t.wins + t.losses)))).length;
                 const message = topTeams.reduce((msg, team) => {
                     const winrate = DiscordUtils_1.DiscordUtils.getPercentString(Math.round(team.wins / (team.losses + team.wins) * 10000) / 100);
                     return msg + DiscordUtils_1.DiscordUtils.fillWithSpaces(String(winrate), 7) + ' | '
-                        + DiscordUtils_1.DiscordUtils.fillWithSpaces(String(team.losses + team.wins), String(Math.max(maxNumberLength, 6)).length) + ' | ' + team.name + '\n';
+                        + DiscordUtils_1.DiscordUtils.fillWithSpaces(String(team.losses + team.wins), Math.max(maxNumberLength, 6)) + ' | ' + team.name + '\n';
                 }, '```Вінрейт | Матчів | Команда\n');
                 msg.channel.send(message + '```');
             });
