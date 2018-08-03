7513f2bcc462ba1c97dd3ec995f47c7177b6f134
SerhiyZheliznjak <zheliznjak@gmail.com>
2018-08-03 13:27:47 +0300

    a

--- src/model/commands/WinRateCommand.js
+++ src/model/commands/WinRateCommand.js
@@ -7,12 +7,12 @@ class WinRate extends Command_1.CommandBase {
         if (!this.isLocked(msg)) {
             this.lock();
             this.dataStore.registeredPlayers.subscribe((registeredPlayers) => {
-                const msgContent = msg.content.toLowerCase();
-                if (this.getArgs(msg.content.toLowerCase()).length > 0) {
+                const args = this.getArgs(msg.content.toLowerCase());
+                if (args.length > 0) {
                     this.dataStore.getHeroes().subscribe(heroes => {
                         let hero_id;
                         const heroNames = Array.from(heroes.keys());
-                        const heroName = heroNames.find(hn => msgContent.indexOf(' ' + hn.toLowerCase()) > -1);
+                        const heroName = heroNames.find(hn => args.indexOf(hn.toLowerCase()) > -1);
                         if (heroName) {
                             hero_id = heroes.get(heroName);
                         }
