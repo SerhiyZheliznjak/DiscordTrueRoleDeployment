6bc7ccaa061671ab3577ad3df9d0ba6b57e73b2b
Serhiy Zheliznyak <zheliznjak@gmail.com>
2018-08-03 20:37:09 +0300

    s

--- src/model/commands/TeamRates.js
+++ src/model/commands/TeamRates.js
@@ -8,7 +8,8 @@ class TeamRates extends Command_1.CommandBase {
                 const digits = this.getArgs(msg.content.toLowerCase()).find(arg => /\d+/.test(arg));
                 const n = !digits ? 10 : +digits.match(/\d+/)[0];
                 const message = teams.slice(0, n).reduce((msg, team) => {
-                    return msg + team.wins / (team.losses + team.wins) + '% | ' + team.losses + team.wins + ' | ' + team.name + '\n';
+                    const winrate = Math.round(team.wins / (team.losses + team.wins) * 10000) / 100;
+                    return msg + winrate + '% | ' + team.losses + team.wins + ' | ' + team.name + '\n';
                 }, '```Вінрейт | Матчів | Команда\n');
                 msg.channel.send(message + '```');
             });
