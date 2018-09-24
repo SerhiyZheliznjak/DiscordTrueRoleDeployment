2f51f6514ba6f89419716151cb70a4e83816cb51
SerhiyZheliznjak <zheliznjak@gmail.com>
2018-09-24 16:47:18 +0300

    dsfdf

--- src/model/nominations/Nenza.js
+++ src/model/nominations/Nenza.js
@@ -42,7 +42,10 @@ class Nenza extends Nomination_1.default {
                     || words.indexOf('pidaras') > -1
                     || words.indexOf('fuck') > -1
                     || words.indexOf('pa') > -1
-                    || words.indexOf('пп') > -1
+                    || words.indexOf('gg') > -1
+                    || words.indexOf('gl') > -1
+                    || words.indexOf('glhf') > -1
+                    || words.indexOf('hf') > -1
                     || words.indexOf('хуй') > -1;
             });
             if (nenzaMsg.length > 0) {
